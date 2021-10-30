d54030876afbec40ac895ce6513a904d7ddd69f6
FlavioAandres <fpareja@condorlabs.io>
2021-10-30 01:25:41 -0500

    Fix budget implementation and improve some performance issues

--- shared/database/repos/user.repo.js
+++ shared/database/repos/user.repo.js
@@ -15,12 +15,12 @@ module.exports.create = async (UserBody = []) => {
 module.exports.getUser = async (searchCriteria, configs = {}, projections = {}) => {
     try {
         await connect();
-        if (configs.banks)
+        if (configs && configs.banks)
             return Users.find({
                 ...searchCriteria
             }).populate('bank')
 
-        return Users.findOne(searchCriteria, { ...projections });
+        return Users.findOne(searchCriteria, projections, configs);
     } catch (e) {
         console.error(e);
         return configs.banks ? [] : {};
@@ -92,7 +92,7 @@ module.exports.updateBudget = async (searchCriteria, update) => {
 
     const [category] = categories.filter(category => category.value === searchCriteria.categories.$elemMatch.value)
 
-    if (category && category.budget.current !== update['categories.$.budget'].current) {
+    if (category && category.budget.current !== update.$set.categories.$.budget.current) {
 
         update['categories.$.budget']['progress'] = (update['categories.$.budget'].current / category.budget.value) * 100
         update['categories.$.budget']['value'] = category.budget.value
@@ -100,4 +100,54 @@ module.exports.updateBudget = async (searchCriteria, update) => {
 
     const result = await userModel.updateOne({ ...searchCriteria }, update)
     return result.nModified > 0
+}
+
+module.exports.updateBudgetFromVars = async (searchValues, update) => {
+    const {
+        categoryValue, 
+        sub: userId, 
+    } = searchValues
+
+    const {
+        currentBudgetValue
+    } = update
+
+    const query = {
+        sub: userId, 
+        categories: {
+            $elemMatch: {
+                value: categoryValue
+            }
+        }
+    }
+
+    const updateDoc = {
+        $set: {
+            'categories.$': {
+                
+            }
+        }
+    }
+    
+    const user = await userModel.findOne(query, { 'categories.$': 1 }, {lean: true});
+    
+    if(!user || !user.categories || !user.categories.length){
+        console.log({ userId, categoryValue, currentBudgetValue, error: "No categories found" })
+        return null; 
+    }
+
+    const category = user.categories[0]
+    if(category && category.budget && category.budget.current !== currentBudgetValue){
+        updateDoc.$set['categories.$'] = {
+            ...category, 
+            budget: {
+                current: currentBudgetValue, 
+                progress: (currentBudgetValue / category.budget.value) * 100,
+                value: category.budget.value, 
+            }
+        }
+        const result = await userModel.updateOne(query, updateDoc)
+        return result.nModified > 0 
+    }
+    return null; 
 }
